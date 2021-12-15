0c0b1883091417378c7032cf5511c8895ac674f9
OS100 <luegera@hotmail.com>
2021-12-15 08:23:27 +0100

    set supply gpios

--- drivers/shtc3/zephyr/shtc3.c
+++ drivers/shtc3/zephyr/shtc3.c
@@ -51,7 +51,7 @@ static int set_supply_i2c(const struct device *dev, bool enable)
 	}
 
 	err = gpio_pin_configure(drv_data->supplyi2c_gpio, SUPPLYI2C_PIN,
-                            GPIO_OUTPUT_INACTIVE | DT_INST_GPIO_FLAGS(0, supplyi2c_gpios));
+                            GPIO_OUTPUT_INIT_LOW | DT_INST_GPIO_FLAGS(0, supplyi2c_gpios));
 
 	if (err!= 0){
 		LOG_INF("GPIO  Supply Config Error");
@@ -254,6 +254,7 @@ static const struct sensor_driver_api shtc3_driver_api = {
 	.channel_get = shtc3_channel_get,
 };
 
+/***************************************************************/
 
 static int shtc3_init(const struct device *dev)
 {
